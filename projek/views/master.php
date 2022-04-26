31839ad2d8bbf7bd95e2f7692c8ccb733d0a31a2
Alruycen <102711026+Alruycen@users.noreply.github.com>
2022-04-26 15:58:04 +0700

    Update master.php

--- projek/views/master.php
+++ projek/views/master.php
@@ -2,4 +2,24 @@
 require '../assets/head.php';
 require '../assets/home.php';
 ?>
-
+<nav class="navbar navbar-expand-md navbar-dark fixed-bottom bg-dark">
+    <div class="container-fluid">
+        <ul class="navbar-nav me-auto mb-2 mb-md-0">
+            <li class="nav-item">
+                <a class="nav-link" href="views/makanan.php">Makanan</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="views/minuman.php">Minuman</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="views/tempat.php">Tempat</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="views/oleholeh.php">Oleh-Oleh</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="views/tentangkami.php">Tentang Kami</a>
+            </li>
+        </ul>
+    </div>
+</nav>
