e6865e03874f37ec38487cd34a8be5a3c0072ac6
Alruycen <102711026+Alruycen@users.noreply.github.com>
2022-04-26 15:54:09 +0700

    Create footer.php

---
+++ projek/assets/footer.php
@@ -0,0 +1,21 @@
+<nav class="navbar navbar-expand-md navbar-dark fixed-bottom bg-dark">
+    <div class="container-fluid">
+        <ul class="navbar-nav me-auto mb-2 mb-md-0">
+            <li class="nav-item">
+                <a class="nav-link" href="makanan.php">Makanan</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="minuman.php">Minuman</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="tempat.php">Tempat</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="oleholeh.php">Oleh-Oleh</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="tentangkami.php">Tentang Kami</a>
+            </li>
+        </ul>
+    </div>
+</nav>
