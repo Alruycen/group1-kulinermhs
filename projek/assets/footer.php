aa5a3c3534bcd549592615059302f5d646819f7a
Alruycen <102711026+Alruycen@users.noreply.github.com>
2022-04-26 17:12:36 +0700

    Update footer.php

--- projek/assets/footer.php
+++ projek/assets/footer.php
@@ -1,5 +1,6 @@
 <nav class="navbar navbar-expand-md navbar-dark fixed-bottom bg-dark">
     <div class="container-fluid">
+        <a class="navbar-brand" href="../index.php">Kuliner Mahasiswa</a>
         <ul class="navbar-nav me-auto mb-2 mb-md-0">
             <li class="nav-item">
                 <a class="nav-link" href="makanan.php">Makanan</a>
@@ -19,3 +20,7 @@
         </ul>
     </div>
 </nav>
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
