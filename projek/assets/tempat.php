5a19de86b465520e0b507611c1251f750baab053
Alruycen <102711026+Alruycen@users.noreply.github.com>
2022-04-27 09:24:06 +0700

    Create tempat.php

---
+++ projek/assets/tempat.php
@@ -0,0 +1,43 @@
+<style>
+    .img-fluid {
+        width:100%;
+        margin-bottom: 50px;
+        height:10rem;
+        object-fit:cover;
+    }
+    body {
+        padding:25px 50px 0 50px;
+        background:#777;
+    }
+    .row {
+        margin-left:5px;
+    }
+    .col-sm-8 {
+        margin-right: 75px;
+        max-width:60%;
+        background: black;
+    }
+    .col-sm-4 {
+        max-width:33%;
+        background:black;
+    }
+</style>
+<body>
+
+    <img class="img-fluid" src="../images/sate-bandeng.jpg" alt="slide1">
+    <div class="row">
+        <div class="col-sm-8">
+            <div class="row mb-2">
+                <div class="col-md-6 bg-white">
+                    <h3 class="mb-0">Featured post</h3>
+                </div>
+                <div class="col-md-6 bg-white">
+                    <h3 class="mb-0">Post title</h3>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-4">
+
+        </div>
+    </div>
+</body>
