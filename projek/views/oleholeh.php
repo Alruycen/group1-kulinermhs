cf8ecbe973e3aed60081394f76df5074a0cfda95
Alruycen <102711026+Alruycen@users.noreply.github.com>
2022-04-27 11:37:14 +0700

    Create oleholeh.php

---
+++ projek/views/oleholeh.php
@@ -0,0 +1,10 @@
+<?php
+if($_GET['page'] =='oleholeh') {
+require '../assets/head.php';
+require '../assets/footer.php';
+require '../assets/oleholeh.php';
+}
+else {
+    header('../index.php');
+}
+?>
