b59e4b66bbb6aef84edb0e92a1dbf45d7a425bf4
Alruycen <102711026+Alruycen@users.noreply.github.com>
2022-04-27 11:37:25 +0700

    Create tentangkami.php

---
+++ projek/views/tentangkami.php
@@ -0,0 +1,10 @@
+<?php
+if($_GET['page'] =='tentangkami') {
+require '../assets/head.php';
+require '../assets/footer.php';
+require '../assets/tentangkami.php';
+}
+else {
+    header('../index.php');
+}
+?>
