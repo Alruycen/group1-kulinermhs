49ffbcaf7040435873e536e94abf3da96c8342b6
Alruycen <102711026+Alruycen@users.noreply.github.com>
2022-04-27 11:37:00 +0700

    Update tempat.php

--- projek/views/tempat.php
+++ projek/views/tempat.php
@@ -1,5 +1,10 @@
 <?php
+if($_GET['page'] =='tempat') {
 require '../assets/head.php';
 require '../assets/footer.php';
 require '../assets/tempat.php';
+}
+else {
+    header('../index.php');
+}
 ?>
