21b44515996960e432e1eb48cba09ce79bf1e474
Alruycen <102711026+Alruycen@users.noreply.github.com>
2022-04-27 12:14:58 +0700

    Update minuman.php

--- projek/views/minuman.php
+++ projek/views/minuman.php
@@ -1,10 +1,3 @@
 <?php
-if($_GET['page'] =='minuman') {
-require '../assets/head.php';
-require '../assets/footer.php';
-require '../assets/minuman.php';
-}
-else {
-    header('../index.php');
-}
+require '../process/switchviews.php';
 ?>
