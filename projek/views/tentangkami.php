be1c6316c8c9f74d1f2ee83e55f752e14383a601
Alruycen <102711026+Alruycen@users.noreply.github.com>
2022-04-27 12:15:44 +0700

    Update tentangkami.php

--- projek/views/tentangkami.php
+++ projek/views/tentangkami.php
@@ -1,10 +1,3 @@
 <?php
-if($_GET['page'] =='tentangkami') {
-require '../assets/head.php';
-require '../assets/footer.php';
-require '../assets/tentangkami.php';
-}
-else {
-    header('../index.php');
-}
+require '../process/switchviews.php';
 ?>
