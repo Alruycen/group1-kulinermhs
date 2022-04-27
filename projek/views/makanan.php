f8a77cc7239105b2cb1787a4f6655b01a5910812
Alruycen <102711026+Alruycen@users.noreply.github.com>
2022-04-27 11:35:24 +0700

    Update makanan.php

--- projek/views/makanan.php
+++ projek/views/makanan.php
@@ -1,7 +1,10 @@
 <?php
-
+if($_GET['page'] =='makanan') {
 require '../assets/head.php';
 require '../assets/footer.php';
 require '../assets/makanan.php';
-
+}
+else {
+    header('../index.php');
+}
 ?>
