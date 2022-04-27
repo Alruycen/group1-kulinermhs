7c69785615ff494f657e6929fb0190dde73cc760
Alruycen <102711026+Alruycen@users.noreply.github.com>
2022-04-27 12:04:34 +0700

    Create tentangkami.php

---
+++ projek/assets/tentangkami.php
@@ -0,0 +1,64 @@
+<title>Tentang Kami</title>
+<style>
+    body {
+        background:#777;
+    }
+    .col {
+        margin:5%;
+        background:blue;
+    }
+    .card-header {
+        background:black;
+        color:white;
+    }
+    .col-6{
+        margin-right:5%;
+    }
+    .row {
+        margin-top:5%;
+    }
+</style>
+<body>
+    <div class="row">
+        <div class="col-6">
+            <div class="card">
+                <div class="card-header">
+                    <h1>Tentang Kami</h1>
+                </div>
+                <div class="card-body">
+                    <div class="card-title">Form: Kritik dan Saran</div>
+                    <form action="#" method="post">
+                        <textarea> </textarea>
+                        <br/>
+                        <button type="submit" class="btn btn-block btn-outline-primary">Kirim</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <div class="col-5">
+            <div class="card">
+                <div class="card-header">
+                    <h5>Para Pengembang</h5>
+                </div>
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-4">
+                            <a href="#"><img class="img-fluid" src="../images/placeholder.png">Profil 1</a>
+                        </div>
+                        <div class="col-4">
+                            <a href="#"><img class="img-fluid" src="../images/placeholder.png">Profil 2</a>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-4">
+                            <a href="#"><img class="img-fluid" src="../images/placeholder.png">Profil 3</a>
+                        </div>
+                        <div class="col-4">
+                            <a href="#"><img class="img-fluid" src="../images/placeholder.png">Profil 4</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
