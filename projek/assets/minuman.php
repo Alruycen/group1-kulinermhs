34c2a5f9b3074ca59349097d8403a0a13c7d46ce
Alruycen <102711026+Alruycen@users.noreply.github.com>
2022-04-27 12:03:17 +0700

    Update minuman.php

--- projek/assets/minuman.php
+++ projek/assets/minuman.php
@@ -1,4 +1,5 @@
 <link rel="stylesheet" href="sidebar/sidebars.css">
+<title>Minuman</title>
 <style>
     body {
         background: #777;
@@ -7,11 +8,11 @@
         max-width:66%;
     }
     .row {
-        padding-top: 50px;
-        padding-left: 50px;
+        padding-top: 5%;
+        padding-left: 5%;
     }
     .col-m-4 {
-        padding-left: 100px;
+        padding-left: 10%;
         max-width:33%;
     }
 </style>
@@ -22,6 +23,9 @@
             <div class="row">
                 <div class="col-sm-6">
                     <div class="card">
+                        <div class="card-header py-3">
+                            <h4 class="my-0 fw-normal">Makanan khas</h4>
+                        </div>
                         <div class="card-body">
                             <h5 class="card-title">Special title treatment</h5>
                             <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
@@ -31,6 +35,9 @@
                 </div>
                 <div class="col-sm-6">
                     <div class="card">
+                        <div class="card-header py-3">
+                            <h4 class="my-0 fw-normal">Makanan khas</h4>
+                        </div>
                         <div class="card-body">
                             <h5 class="card-title">Special title treatment</h5>
                             <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
@@ -42,6 +49,9 @@
             <div class="row">
                 <div class="col-sm-4">
                     <div class="card">
+                        <div class="card-header py-3">
+                            <h4 class="my-0 fw-normal">Makanan khas</h4>
+                        </div>
                         <div class="card-body">
                             <h5 class="card-title">Special title treatment</h5>
                             <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
@@ -51,6 +61,9 @@
                 </div>
                 <div class="col-sm-4">
                     <div class="card">
+                        <div class="card-header py-3">
+                            <h4 class="my-0 fw-normal">Makanan khas</h4>
+                        </div>
                         <div class="card-body">
                             <h5 class="card-title">Special title treatment</h5>
                             <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
@@ -60,6 +73,9 @@
                 </div>
                 <div class="col-sm-4">
                     <div class="card">
+                        <div class="card-header py-3">
+                            <h4 class="my-0 fw-normal">Makanan khas</h4>
+                        </div>
                         <div class="card-body">
                             <h5 class="card-title">Special title treatment</h5>
                             <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
@@ -72,6 +88,10 @@
         <div class="col-m-4">
             <div class="card">
                 <div class="card-body">
+                    <form class="d-flex">
+                        <input class="form-control me-2" type="search" placeholder="Makanan khas Tangerang" aria-label="Search">
+                        <button class="btn btn-outline-success" type="submit">Cari</button>
+                    </form>
                     <h5 class="card-title">Card title</h5>
                     <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
