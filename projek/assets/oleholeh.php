078fd69ce5b822a0d4c990ea0c3053eb9fa1799d
Alruycen <102711026+Alruycen@users.noreply.github.com>
2022-04-27 12:04:21 +0700

    Create oleholeh.php

---
+++ projek/assets/oleholeh.php
@@ -0,0 +1,71 @@
+<title>Oleh-Oleh</title>
+<style>
+    body {
+        background: #777;
+    }
+    .d-flex {
+        padding: 5% 2.5% 0 2.5%;
+    }
+    .col-6 {
+        padding: 5%;
+    }
+</style>
+
+<body>
+    <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Makanan khas Tangerang" aria-label="Search">
+        <button class="btn btn-outline-light" type="submit">Cari</button>
+    </form>
+    <div class="row">
+        <div class="col-6">
+            <div class="card">
+                <div class="card-header">
+                    <h5>Oleh-Oleh Khas Tangerang</h5>
+                </div>
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-6">
+                            <a href="#"><img class="img-fluid" src="../images/placeholder.png"></a>
+                        </div>
+                        <div class="col-6">
+                            <a href="#"><img class="img-fluid" src="../images/placeholder.png"></a>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-6">
+                            <a href="#"><img class="img-fluid" src="../images/placeholder.png"></a>
+                        </div>
+                        <div class="col-6">
+                            <a href="#"><img class="img-fluid" src="../images/placeholder.png"></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-6">
+            <div class="card">
+                <div class="card-header">
+                    <h5>Kios Jual Oleh-Oleh</h5>
+                </div>
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-6">
+                            <a href="#"><img class="img-fluid" src="../images/placeholder.png"></a>
+                        </div>
+                        <div class="col-6">
+                            <a href="#"><img class="img-fluid" src="../images/placeholder.png"></a>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-6">
+                            <a href="#"><img class="img-fluid" src="../images/placeholder.png"></a>
+                        </div>
+                        <div class="col-6">
+                            <a href="#"><img class="img-fluid" src="../images/placeholder.png"></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
