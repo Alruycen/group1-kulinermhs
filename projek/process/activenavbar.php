17f6f54b334a0e54f3c3160c138111fc55b7b0b0
Alruycen <102711026+Alruycen@users.noreply.github.com>
2022-04-29 16:12:22 +0700

    Update activenavbar.php

--- projek/process/activenavbar.php
+++ projek/process/activenavbar.php
@@ -5,33 +5,53 @@
     else
         $page = $_GET['page'];
 ?>
-<li class="<?php 
-    if($page =='makanan') 
-        echo 'nav-item active'; 
-    else echo 'nav-item';?>">
-    <a class="nav-link" href="../views/makanan.php?page=makanan">Makanan</a>
+<li class="nav-item">
+    <a class="<?php
+        if($page =='makanan')
+            echo 'nav-link active'; 
+        else echo 'nav-link';?>" 
+    href="<?php
+        if($page =='makanan') 
+            echo '../index.php'; 
+        else echo '../views/menu.php?page=makanan'; ?>">Makanan</a>
 </li>
-<li class="<?php 
-    if($page =='minuman') 
-        echo 'nav-item active'; 
-    else echo 'nav-item';?>">
-    <a class="nav-link" href="../views/minuman.php?page=minuman">Minuman</a>
+<li class="nav-item">
+    <a class="<?php
+        if($page =='minuman')
+            echo 'nav-link active'; 
+        else echo 'nav-link';?>" 
+    href="<?php
+        if($page =='minuman') 
+            echo '../index.php'; 
+        else echo '../views/menu.php?page=minuman'; ?>">Minuman</a>
 </li>
-<li class="<?php 
-    if($page =='tempat') 
-        echo 'nav-item active'; 
-    else echo 'nav-item';?>">
-    <a class="nav-link" href="../views/tempat.php?page=tempat">Tempat</a>
+<li class="nav-item">
+    <a class="<?php
+        if($page =='tempat')
+            echo 'nav-link active'; 
+        else echo 'nav-link';?>" 
+    href="<?php
+        if($page =='tempat') 
+            echo '../index.php'; 
+        else echo '../views/menu.php?page=tempat'; ?>">Tempat</a>
 </li>
-<li class="<?php 
-    if($page =='oleholeh') 
-        echo 'nav-item active'; 
-    else echo 'nav-item';?>">
-    <a class="nav-link" href="../views/oleholeh.php?page=oleholeh">Oleh-Oleh</a>
+<li class="nav-item">
+    <a class="<?php
+        if($page =='oleholeh')
+            echo 'nav-link active'; 
+        else echo 'nav-link';?>" 
+    href="<?php
+        if($page =='oleholeh') 
+            echo '../index.php'; 
+        else echo '../views/menu.php?page=oleholeh'; ?>">Oleh-Oleh</a>
 </li>
-<li class="<?php 
-    if($page =='tentangkami') 
-        echo 'nav-item active'; 
-    else echo 'nav-item';?>">
-    <a class="nav-link" href="../views/tentangkami.php?page=tentangkami">Tentang Kami</a>
-</li>
+<li class="nav-item">
+    <a class="<?php
+        if($page =='tentangkami')
+            echo 'nav-link active'; 
+        else echo 'nav-link';?>" 
+    href="<?php
+        if($page =='tentangkami') 
+            echo '../index.php'; 
+        else echo '../views/menu.php?page=tentangkami'; ?>">Tentang Kami</a>
+</li>z
