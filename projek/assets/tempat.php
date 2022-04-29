51bd809635f477a23e0c4d4029e853132369194c
Alruycen <102711026+Alruycen@users.noreply.github.com>
2022-04-29 15:46:34 +0700

    Update tempat.php

--- projek/assets/tempat.php
+++ projek/assets/tempat.php
@@ -10,37 +10,83 @@
         padding:2.5% 5% 0 5%;
         background:#777;
     }
-    .row {
-        margin-left:0.5%;
+    .list-group{
+        position: sticky;
+        top: 1%;
     }
-    .col-sm-8 {
-        margin-right: 6%;
-        max-width:60%;
-        background: black;
+    .col-sm-5 {
+        margin-right:7.6%;
+        max-height:16rem;
+        overflow-y:scroll;
     }
     .col-sm-4 {
-        max-width:33%;
-        background:black;
+        max-height:16rem;
+        overflow-y:scroll;
+    }
+    .dropdown-header {
+        margin-bottom:5%;
+    }
+    .active {
+        border-bottom: 3px solid #fff;
+    }
+    .nav-link:hover {
+        border-bottom: 1px solid #fff;
     }
 </style>
-<body>
-
+<body data-bs-spy="scroll" data-bs-target="#myScrollspy">
     <img class="img-fluid" src="../images/sate-bandeng.jpg" alt="slide1">
     <div class="row">
-        <div class="col-sm-8">
-            <div class="row mb-2">
-                <div class="col-md-6 bg-white">
-                    <h3 class="mb-0">Featured post</h3>
-                </div>
-                <div class="col-md-6 bg-white">
-                    <h3 class="mb-0">Post title</h3>
+        <div class="col-sm-2 bg-info" id="myScrollspy">
+            <btn class="btn d-block bg-white">Pilih</btn>
+            <div class="list-group">
+                <a class="list-group-item list-group-item-action active" href="#section1">Pasar Malam</a>
+                <a class="list-group-item list-group-item-action" href="#section2">Kafe</a>
+                <a class="list-group-item list-group-item-action" href="#section3">Restoran</a>
+            </div>
+        </div>
+        <div class="col-sm-5 bg-info">
+            <div class="card">
+                <div class="card-body">
+                    <div id="section1">
+		                <div id="section1">
+                        </div>
+		                <div id="section2">
+                        </div>
+		                <div id="section3">
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="col-sm-4">
+        <div class="col-sm-4 bg-black">
             <?php
                 require 'formsearch.php';
             ?>
+            <h6 class="bg-white">Rating dan Ulasan</h5>
         </div>
     </div>
 </body>
+<!--<div class="col-sm-6">
+            <div class="row row-cols-2">
+                <div class="col-md-4" id="myScrollspy">
+                    <btn class="btn d-block bg-white">Pilih</btn>
+                    <div class="list-group">
+                        <a class="list-group-item list-group-item-action active" href="#section1">Pasar Malam</a>
+                        <a class="list-group-item list-group-item-action" href="#section2">Kafe</a>
+                        <a class="list-group-item list-group-item-action" href="#section3">Restoran</a>
+                    </div>
+                </div>
+                <div class="col-md-8 bg-info">
+                    <div class="card">
+                        <div class="card-body">
+                            <div id="section1">
+				                <div id="section1">
+                                </div>
+			                    <div id="section2">
+                                </div>
+			                    <div id="section3">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>-->
