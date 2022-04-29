0cb062799e2e8a391f451f8488647f44a79aa193
Alruycen <102711026+Alruycen@users.noreply.github.com>
2022-04-29 15:45:34 +0700

    Update minuman.php

--- projek/assets/minuman.php
+++ projek/assets/minuman.php
@@ -5,15 +5,23 @@
         background: #777;
     }
     .col-m-8 {
+        padding-left:5%;
+        padding-top:5%;
         max-width:66%;
     }
-    .row {
-        padding-top: 5%;
-        padding-left: 5%;
-    }
     .col-m-4 {
+        padding-top: 5%;
         padding-left: 10%;
         max-width:33%;
+    }
+    .col-sm-6 {
+        padding: 0 5% 5% 0;
+    }
+    .active {
+        border-bottom: 3px solid #fff;
+    }
+    .nav-link:hover {
+        border-bottom: 1px solid #fff;
     }
 </style>
 
@@ -24,21 +32,17 @@
                 <div class="col-sm-6">
                     <div class="card">
                         <div class="card-header py-3">
-                            <h4 class="my-0 fw-normal">Makanan khas</h4>
+                            <h4 class="my-0 fw-normal">Resep</h4>
                         </div>
-                        <div class="card-body">
-                           <a href="#"><img class="img-fluid" src="../images/placeholder.png"></a>
-                        </div>
+                        <a href="#" class="stretched-link"><img class="card-img-top" src="../images/placeholder.png"></a>
                     </div>
                 </div>
                 <div class="col-sm-6">
                     <div class="card">
                         <div class="card-header py-3">
-                            <h4 class="my-0 fw-normal">Makanan khas</h4>
+                            <h4 class="my-0 fw-normal">Minuman viral</h4>
                         </div>
-                        <div class="card-body">
-                           <a href="#"><img class="img-fluid" src="../images/placeholder.png"></a>
-                        </div>
+                        <a href="#" class="stretched-link"><img class="card-img-top" src="../images/placeholder.png"></a>
                     </div>
                 </div>
             </div>
@@ -46,31 +50,25 @@
                 <div class="col-sm-4">
                     <div class="card">
                         <div class="card-header py-3">
-                            <h4 class="my-0 fw-normal">Makanan khas</h4>
+                            <h4 class="my-0 fw-normal">Minuman khas</h4>
                         </div>
-                        <div class="card-body">
-                           <a href="#"><img class="img-fluid" src="../images/placeholder.png"></a>
-                        </div>
+                        <a href="../views/artikel.php?page=minuman" class="stretched-link"><img class="card-img-top" src="../images/placeholder.png"></a>
                     </div>
                 </div>
                 <div class="col-sm-4">
                     <div class="card">
                         <div class="card-header py-3">
-                            <h4 class="my-0 fw-normal">Makanan khas</h4>
+                            <h4 class="my-0 fw-normal">Rentang harga</h4>
                         </div>
-                        <div class="card-body">
-                           <a href="#"><img class="img-fluid" src="../images/placeholder.png"></a>
-                        </div>
+                        <a href="#" class="stretched-link"><img class="card-img-top" src="../images/placeholder.png"></a>
                     </div>
                 </div>
                 <div class="col-sm-4">
                     <div class="card">
                         <div class="card-header py-3">
-                            <h4 class="my-0 fw-normal">Makanan khas</h4>
+                            <h4 class="my-0 fw-normal">Rating & Review</h4>
                         </div>
-                        <div class="card-body">
-                           <a href="#"><img class="img-fluid" src="../images/placeholder.png"></a>
-                        </div>
+                        <a href="#" class="stretched-link"><img class="card-img-top" src="../images/placeholder.png"></a>
                     </div>
                 </div>
             </div>
@@ -81,11 +79,22 @@
                     <?php
                         require 'formsearch.php';
                     ?>
-                    <h5 class="card-title">Card title</h5>
-                    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="card-link">Card link</a>
-                    <a href="#" class="card-link">Another link</a>
+                    <ul>
+                        <li><h5 class="card-title">Artikel Terbaru</h5></li>
+                        <a class="card-link" href="#"><img class="card-img mb-2" src="../images/placeholder.png" alt="artikel terbaru"></a>
+                        <li><h5 class="card-title">Yang Paling Diminati</h5></li>
+                        <a class="card-link" href="#"><img class="card-img mb-2" src="../images/placeholder.png" alt="yang paling diminati"></a>
+                    </ul>
+                    <h6 class="card-subtitle">Minuman khas</h6>
+                    <ul>
+                        <li><a class ="card-link" href="#">Es kuwut</a></li>
+                        <li><a class ="card-link" href="#">Bir pletok</a></li>
+                    </ul>
+                    <h6 class="card-subtitle">Cari tempat nongkrong?</h6>
+                    
+                    <ul>
+                        <li><a class ="card-link" href="#">Cek di tempat kafe</a></li>
+                    </ul>
                 </div>
             </div>
         </div>
