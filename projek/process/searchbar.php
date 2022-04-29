4bad97824ba3d47439adf3cbda400bdac89c6d14
Alruycen <102711026+Alruycen@users.noreply.github.com>
2022-04-29 16:12:47 +0700

    Update searchbar.php

--- projek/process/searchbar.php
+++ projek/process/searchbar.php
@@ -59,11 +59,11 @@ else {
                 $foto = '../images/placeholder.png';
             }
             echo "<div class='row'>
-                    <div class='col-2'>
+                    <div class='col-6'>
                         <h5>".$nama."</h5>
                         <img class='img-fluid' src='".$foto."'>
                     </div>
-                    <div class='col-2'>
+                    <div class='col-4'>
                         <div class='scrolling'>
                             <p>".$deskripsi."</p>
                         </div>
