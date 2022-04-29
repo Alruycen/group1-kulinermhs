8ae16f6355e6587c89c07c7c395709f4f0fc6ced
Alruycen <102711026+Alruycen@users.noreply.github.com>
2022-04-29 15:44:52 +0700

    Create formsearch.php

---
+++ projek/assets/formsearch.php
@@ -0,0 +1,11 @@
+<form class="d-flex" action="../views/artikel.php" method="post">
+    <select class="form-select w-20" name="page">
+        <option selected>...</option>
+        <option value="1">Makanan</option>
+        <option value="2">Minuman</option>
+        <option value="3">Tempat</option>
+        <option value="4">Oleh-Oleh</option>
+    </select>
+    <input class="form-control me-2" type="text" name="search" placeholder="Makanan khas Tangerang" aria-label="Search">
+    <button class="btn btn-outline-success" type="submit" name="submit-search">Cari</button>
+</form>
