eb39cde28002009c9c89b1e6d4baa93fe7d16a65
Alruycen <102711026+Alruycen@users.noreply.github.com>
2022-04-29 15:44:19 +0700

    Update artikel.php

--- projek/assets/artikel.php
+++ projek/assets/artikel.php
@@ -8,20 +8,32 @@
         margin-bottom:5%;
     }
     .col-m-4 {
-        padding-top:5%;
+        padding-top:1%;
         max-width:30%;
     }
     .row .row {
         background:#777;
+        padding:5%;
     }
     .col-6 {
-        color: white;
-        background:#white;
+        background:white;
+        padding-right:5%;
     }
     .col-4 {
+        padding-left:5%;
+        margin-left:10%;
+        position:relative;
+        padding-top:1%;
         background:peachpuff;
+        height:100%;
+        overflow-y:scroll;
     }
-
+    .active {
+        border-bottom: 3px solid #fff;
+    }
+    .nav-link:hover {
+        border-bottom: 1px solid #fff;
+    }
 </style>
 <body>
     <div class="row">
@@ -33,15 +45,26 @@
         <div class="col-m-4">
             <div class="card">
                 <div class="card-body">
-                    <form class="d-flex" action="../process/searchbar.php" method="post">
-                        <input class="form-control me-2" type="text" name="search" placeholder="Makanan khas Tangerang" aria-label="Search">
-                        <button class="btn btn-outline-success" type="submit" name="submit-search">Cari</button>
-                    </form>
-                    <h5 class="card-title">Card title</h5>
-                    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="card-link">Card link</a>
-                    <a href="#" class="card-link">Another link</a>
+                    <?php
+                        require 'formsearch.php';
+                    ?>
+                    <ul>
+                        <li><h5 class="card-title">Artikel Terbaru</h5></li>
+                        <a class="card-link" href="#"><img class="card-img mb-2" src="../images/placeholder.png" alt="artikel terbaru"></a>
+                        <li><h5 class="card-title">Yang Paling Diminati</h5></li>
+                        <a class="card-link" href="#"><img class="card-img mb-2" src="../images/placeholder.png" alt="yang paling diminati"></a>
+                    </ul>
+                    <h6 class="card-subtitle">Makanan khas</h6>
+                    <ul>
+                        <li><a class ="card-link" href="#">Soto Betawi</a></li>
+                        <li><a class ="card-link" href="#">Sate</a></li>
+                    </ul>
+                    <h6 class="card-subtitle">Cari Jajanan?</h6>
+                    
+                    <ul>
+                        <li><a class ="card-link" href="#">Cek di oleh-oleh khas</a></li>
+                        <li><a class ="card-link" href="#">Cek di tempat pasar malam</a></li>
+                    </ul>
                 </div>
             </div>
         </div>
