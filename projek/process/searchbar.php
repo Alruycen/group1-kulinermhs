704167854105fdad7a469b89327259b1b10b85a8
Alruycen <102711026+Alruycen@users.noreply.github.com>
2022-05-01 15:47:18 +0700

    Update searchbar.php

--- projek/process/searchbar.php
+++ projek/process/searchbar.php
@@ -1,80 +1,60 @@
 <?php
     include 'function.php';
-
-if (isset($_POST['submit-search'])) {
     try {
-        $sql = "SELECT * FROM artikel WHERE nama LIKE ? OR deskripsi LIKE ? OR penulis LIKE ?";
-
-        $stmt = $conn->prepare($sql);
-        $stmt->execute(["%$_POST[search]%", "%$_POST[search]%", "%$_POST[search]%"]);
-
-        while ($data = $stmt->fetch(PDO::FETCH_ASSOC)) {
-            $nama = $data['nama'];
-            $deskripsi = $data['deskripsi'];
-            $foto;
-            if (file_exists($data['foto'])) {
-                $foto = $data['foto'];
-            }
-            else {
-                $foto = '../images/placeholder.png';
-            }
-            echo "<div class='row'>
-                    <div class='col-6'>
-                        <h5>".$nama."</h5>
-                        <img class='img-fluid' src='".$foto."'>
-                    </div>
-                    <div class='col-4'>
-                        <div class='scrolling'>
-                            <p>".$deskripsi."</p>
-                        </div>
-                    </div>
-                </div>";
-        }
-
-    }
-    catch (Exception $e) {
-        echo "<pre>";
-        var_dump($e);
-    }
-
-
-}
-else {
-    try {
+        $submenu = "";
         $conn->setAttribute(PDO::ATTR_ERRMODE,
-            PDO::ERRMODE_EXCEPTION);
-
-        $sql = "SELECT * FROM artikel";
-
-        $stmt = $conn->query($sql);
-
-        while ($data = $stmt->fetch(PDO::FETCH_ASSOC)) {
-            $nama = $data['nama'];
-            $deskripsi = $data['deskripsi'];
-            $foto;
-            if (file_exists($data['foto'])) {
-                $foto = $data['foto'];
+                    PDO::ERRMODE_EXCEPTION);
+        if (isset($_POST['submit-search'])) {
+            $search = $_POST['search'];
+            $menu = [1, 2, 3, 4];
+            if (isset($_POST['page']) && in_array($_POST['page'], $menu)) {
+                $submenu = $_POST['page'];
+                $sql = "SELECT * FROM artikel WHERE id_kategori = ? AND (nama LIKE ? OR deskripsi LIKE ? OR penulis LIKE ?)";
+                $atrb = [$submenu, "%$search%", "%$search%", "%$search%"];
             }
             else {
-                $foto = '../images/placeholder.png';
-            }
-            echo "<div class='row'>
-                    <div class='col-6'>
-                        <h5>".$nama."</h5>
-                        <img class='img-fluid' src='".$foto."'>
-                    </div>
-                    <div class='col-4'>
-                        <div class='scrolling'>
-                            <p>".$deskripsi."</p>
-                        </div>
-                    </div>
-                </div>";
-        }
+                $sql = "SELECT * FROM artikel WHERE nama LIKE ? OR deskripsi LIKE ? OR penulis LIKE ?";
+                $atrb = ["%$search%", "%$search%", "%$search%"];
+            }   
+            $stmt = $conn->prepare($sql);
+            $stmt->execute($atrb);
 
+            switch($submenu) {
+                case "1": $submenu = "makanan"; break;
+                case "2": $submenu = "minuman"; break;
+                case "3": $submenu = "tempat"; break;
+                case "4": $submenu = "oleholeh"; break;
+                default: $submenu = "";
+                    break;
+            }
+        }
+        else {
+            switch($_GET['page']) {
+                case "makanan":
+                    $sql = "SELECT * FROM artikel WHERE id = 1";
+                    $submenu = "makanan";
+                    break;
+                case "minuman":
+                    $sql = "SELECT * FROM artikel WHERE id = 2";
+                    $submenu = "minuman";
+                    break;
+                case "tempat":
+                    $sql = "SELECT * FROM artikel WHERE id = 3";
+                    $submenu = "tempat";
+                    break;
+                case "oleholeh":
+                    $sql = "SELECT * FROM artikel WHERE id = 4";
+                    $submenu = "oleholeh";
+                    break;
+                default:
+                    header('Location: ../index.php');
+                    break;
+            }
+            $stmt = $conn->query($sql);
+        }
     }
     catch (Exception $e) {
         echo "<pre>";
         var_dump($e);
     }
-}
 ?>
