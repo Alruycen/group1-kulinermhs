db1b84d33fbaa9fed73450025fdab5c89fe26b3f
Alruycen <102711026+Alruycen@users.noreply.github.com>
2022-05-02 15:50:41 +0700

    Create formtempat.php

---
+++ projek/assets/formtempat.php
@@ -0,0 +1,10 @@
+<form class="d-flex" action="../views/menu.php?page=tempat" method="post">
+    <select class="form-select w-20" name="tempat">
+        <option selected>...</option>
+        <option value="1">Pasar Malam</option>
+        <option value="2">Kafe</option>
+        <option value="3">Restoran</option>
+    </select>
+    <input class="form-control me-2" type="text" name="search" placeholder="Makanan khas Tangerang" aria-label="Search">
+    <button class="btn btn-outline-success" type="submit" name="submit-search">Cari</button>
+</form>
