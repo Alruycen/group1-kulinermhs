8d131e1f2e6d79b8db5812f3a0a22a96e420e7a6
Alruycen <102711026+Alruycen@users.noreply.github.com>
2022-05-03 16:15:17 +0700

    Update oleholeh.php

--- projek/assets/oleholeh.php
+++ projek/assets/oleholeh.php
@@ -2,16 +2,31 @@
 <style>
     body {
         background: #777;
+        overflow-x: clip;
     }
+
     .d-flex {
         padding: 5% 2.5% 0 2.5%;
     }
+
     .col-6 {
         padding: 5%;
     }
+
+    .card-header {
+        height: 4rem;
+    }
+
+    .scrolling {
+        height: 8rem;
+        overflow-y: scroll;
+        overflow-x: clip;
+    }
+
     .active {
         border-bottom: 3px solid #fff;
     }
+
     .nav-link:hover {
         border-bottom: 1px solid #fff;
     }
@@ -19,7 +34,7 @@
 
 <body>
     <?php
-        require 'formsearch.php';
+    require 'formoleh.php';
     ?>
     <div class="row">
         <div class="col-6">
@@ -28,22 +43,31 @@
                     <h5>Oleh-Oleh Khas Tangerang</h5>
                 </div>
                 <div class="card-body">
-                    <div class="row">
-                        <div class="col-6">
-                            <a href="#"><img class="img-fluid" src="../images/placeholder.png"></a>
+                    <?php 
+                    include '../process/proses_oleh.php';
+                    $i = 0;
+                    while ($data = $stmt->fetch()) :
+                        if (File_exists($data['foto'])) {
+                            $foto = $data['foto'];
+                        } else {
+                            $foto = '../images/placeholder.png';
+                        }
+                        $i++;
+                        if ($i % 2 == 1) echo "<div class='row row-cols-2'>"; ?>
+                        <div class="col">
+                            <div class="card">
+                                <div class="card-header">
+                                    <h6><?= $data['nama']; ?></h6>
+                                </div>
+                                <img src="<?= $foto; ?>" class="card-img-top">
+                                <div class="scrolling">
+                                    <p><em><?= $data['deskripsi']; ?></em></p>
+                                </div>
+                            </div>
                         </div>
-                        <div class="col-6">
-                            <a href="#"><img class="img-fluid" src="../images/placeholder.png"></a>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-6">
-                            <a href="#"><img class="img-fluid" src="../images/placeholder.png"></a>
-                        </div>
-                        <div class="col-6">
-                            <a href="#"><img class="img-fluid" src="../images/placeholder.png"></a>
-                        </div>
-                    </div>
+                        <?php if ($i % 2 == 0 && $i % 2 != 1) echo "</div>"; ?>
+                    <?php endwhile; ?>
+                    <?php if ($i == 1) echo "</div>"; ?>
                 </div>
             </div>
         </div>
@@ -53,22 +77,44 @@
                     <h5>Kios Jual Oleh-Oleh</h5>
                 </div>
                 <div class="card-body">
-                    <div class="row">
-                        <div class="col-6">
-                            <a href="#"><img class="img-fluid" src="../images/placeholder.png"></a>
+                    <?php $i = 0;
+                    while ($data = $stmt2->fetch()) :
+                        if (File_exists($data['foto'])) {
+                            $foto = $data['foto'];
+                        } else {
+                            $foto = '../images/placeholder.png';
+                        }
+                        $i++;
+                        if ($i % 2 == 1) echo "<div class='row row-cols-2'>"; ?>
+                        <div class="col">
+                            <div class="card">
+                                <div class="card-header">
+                                    <h6><?= $data['merek']; ?></h6>
+                                </div>
+                                <img src="<?= $foto; ?>" class="card-img-top">
+                                <div class="scrolling">
+                                    <dl class="row">
+                                        <dt class="col-4">
+                                            <p><strong>Harga</strong></p>
+                                        </dt>
+                                        <dd class="col-8">
+                                            <p>Rp <em><?= $data['hargaminimal']; ?></em>-<em><?= $data['hargamaksimal']; ?></em></p>
+                                        </dd>
+                                    </dl>
+                                    <dl class="row">
+                                        <dt class="col-4">
+                                            <p><strong>Lokasi</strong></p>
+                                        </dt>
+                                        <dd class="col-8">
+                                            <p><?= $data['lokasi']; ?></p>
+                                        </dd>
+                                    </dl>
+                                </div>
+                            </div>
                         </div>
-                        <div class="col-6">
-                            <a href="#"><img class="img-fluid" src="../images/placeholder.png"></a>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-6">
-                            <a href="#"><img class="img-fluid" src="../images/placeholder.png"></a>
-                        </div>
-                        <div class="col-6">
-                            <a href="#"><img class="img-fluid" src="../images/placeholder.png"></a>
-                        </div>
-                    </div>
+                        <?php if ($i % 2 == 0 && $i % 2 != 1) echo "</div>"; ?>
+                    <?php endwhile; ?>
+                    <?php if ($i == 1) echo "</div>"; ?>
                 </div>
             </div>
         </div>
