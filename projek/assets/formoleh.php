b046c81d574626d8a2c6077a8def3264422bb9ff
Alruycen <102711026+Alruycen@users.noreply.github.com>
2022-05-03 16:12:24 +0700

    Create formoleh.php

---
+++ projek/assets/formoleh.php
@@ -0,0 +1,4 @@
+<form class="d-flex" action="../views/menu.php?page=oleholeh" method="post">
+    <input class="form-control me-2" type="text" name="search" placeholder="Makanan khas Tangerang" aria-label="Search">
+    <button class="btn btn-outline-success" type="submit" name="submit-search">Cari</button>
+</form>
