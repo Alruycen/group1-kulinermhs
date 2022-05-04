0bb2ed0a78c27e0cce3af2c290e6e47d1bde13e6
Alruycen <102711026+Alruycen@users.noreply.github.com>
2022-05-04 15:48:38 +0700

    Update formartikel.php

--- projek/assets/formartikel.php
+++ projek/assets/formartikel.php
@@ -1,11 +1,11 @@
 <form class="d-flex" action="../views/artikel.php?page=" method="post">
-    <select class="form-select w-20" name="page">
-        <option selected>...</option>
+    <select name="page" class="form-control">
+        <option value="">Pilih</option>
         <option value="1">Makanan</option>
         <option value="2">Minuman</option>
         <option value="3">Tempat</option>
-        <option value="4">Oleh-Oleh</option>
+        <option value="4">Oleh-oleh</option>
     </select>
-    <input class="form-control me-2" type="text" name="search" placeholder="Makanan khas Tangerang" aria-label="Search">
+    <input class="form-control me-2" type="text" id="search" name="search" placeholder="Makanan khas Tangerang" aria-label="Search">
     <button class="btn btn-outline-success" type="submit" name="submit-search">Cari</button>
 </form>
