a6283dcb5e25957eee2ba2906fd9e0c96fc2cd76
Alruycen <102711026+Alruycen@users.noreply.github.com>
2022-05-07 14:10:43 +0700

    Delete projek/assets directory

--- projek/assets/footer.php
+++
@@ -1,5 +0,0 @@
-<nav class="navbar navbar-expand-lg navbar-dark fixed-bottom bg-dark">
-    <div class="container-fluid">
-            <?php include '../process/activenavbar.php'; ?>
-    </div>
-</nav>
--- projek/assets/formoleh.php
+++
@@ -1,4 +0,0 @@
-<form class="d-flex" action="../views/menu.php?page=oleholeh" method="post">
-    <input class="form-control me-2" type="text" name="search" placeholder="Makanan khas Tangerang" aria-label="Search">
-    <button class="btn btn-outline-success" type="submit" name="submit-search">Cari</button>
-</form>
